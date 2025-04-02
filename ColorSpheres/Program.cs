8e9f559eb38b384de3cc43324c7f118e28368722
Rodrigo <rodrigogomes091997@gmail.com>
2025-04-02 23:11:45 +0100

    Colors + Getters

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ColorSpheres
+{
+    public class Color
+    {
+        private byte Red, Green, Blue, Alpha;
+        public Color(byte red, byte green, byte blue, byte alpha = 255)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+        }
+        
+        // Getters for the color components
+        public byte GetRed() => Red;
+        public byte GetGreen() => Green;
+        public byte GetBlue() => Blue;
+        public byte GetAlpha() => Alpha;
+    }
+}
