c8fa9ee8efff5705e8ec4c93b0ba79ce6c498af4
Rodrigo <rodrigogomes091997@gmail.com>
2025-04-03 18:23:32 +0100

    Add HighScore property

--- PlayerStats/Program.cs
+++ PlayerStats/Program.cs
@@ -7,5 +7,18 @@ namespace PlayerStats
         private float highScore; // Stores the high score
         private int playedGames; // Number of games played
         private int wonGames; // Number of games won
+
+        // High score property (updates only if the new score is higher)
+        public float HighScore
+        {
+            get => highScore;
+            set
+            {
+                if (value > highScore)
+                {
+                    highScore = value;
+                }
+            }
+        }
     }
 }
