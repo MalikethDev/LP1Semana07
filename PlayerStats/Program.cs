d70524f9be6a6eea73cfd9230198c045bb04df40
Rodrigo <rodrigogomes091997@gmail.com>
2025-04-03 18:11:07 +0100

    Add PlayerStats

---
+++ PlayerStats/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PlayerStats
+{
+    public class Player
+    {
+        private float highScore; // Stores the high score
+        private int playedGames; // Number of games played
+        private int wonGames; // Number of games won
+    }
+}
